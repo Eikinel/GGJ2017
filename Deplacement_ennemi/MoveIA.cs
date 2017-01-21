85bea4289ea4462960a237c50cfa52679a5bffd9
Antoine Valente <antoine.valente@epitech.eu>
2017-01-21 01:45:07 +0100

    Script deplacement joueur + ennemi

---
+++ Deplacement_ennemi/MoveIA.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveIA : MonoBehaviour
+{
+    public float _Speed;
+    private MovePlayer Player;
+
+    // Use this for initialization
+    void Start()
+    {
+        Player = GameObject.Find("player").GetComponent<MovePlayer>();
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, Player.PosPlayer, _Speed * Time.deltaTime);
+    }
+}
---
+++ Deplacement_ennemi/MovePlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+    public float _Speed;
+    public Vector3 PosPlayer;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float _movex = Input.GetAxis("Horizontal");
+        float _movey = Input.GetAxis("Vertical");
+        PosPlayer = transform.position;
+        GetComponent<Rigidbody2D>().velocity = new Vector3(_movex * _Speed, _movey * _Speed, 0);
+    }
+}
