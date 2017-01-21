7f8d49adc43a09a90f7a4e774f0c9657ced389fb
Eikinel <valentin.pierrel@epitech.eu>
2017-01-21 22:41:54 +0100

    Waves fini

---
+++ Assets/Scripts/HitGround.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitGround : MonoBehaviour {
+
+    public  GameObject  _wave;
+    private float       _direction;
+    private Vector3     _wave_pos;
+
+    // Use this for initialization
+    void Start ()
+    {
+        if (transform.tag == "Left Fist")
+            _direction = -1;
+        else if (transform.tag == "Right Fist")
+            _direction = 1;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Plateform")
+        {
+            _wave_pos.x = this.transform.position.x + this.GetComponent<BoxCollider2D>().bounds.size.x * -_direction;
+            _wave_pos.y = GameObject.Find("plateform").GetComponent<BoxCollider2D>().transform.position.y + this.GetComponent<BoxCollider2D>().bounds.size.y / 2;
+            _wave_pos.z = 1f;
+            Instantiate(_wave, _wave_pos, new Quaternion(0, (_direction + 1) / 2, 0, 0), this.transform);
+        }
+    }
+}
---
+++ Assets/Scripts/Wave.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wave : MonoBehaviour {
+
+    public  float       _speed;
+    private float       _direction;
+    private Vector3     _vect;
+
+	// Use this for initialization
+	void Start ()
+    {
+        _vect = new Vector3(_speed * Time.deltaTime, 0, 0);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.Translate(_vect);
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.tag == "Player")
+            //Ici enlever de la vie au joueur
+            Debug.Log("Collision  T R I G G E R");
+    }
+}
